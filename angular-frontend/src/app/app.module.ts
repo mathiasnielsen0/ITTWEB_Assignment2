9261a142edf5a6e9213de31ee5e06338f9d74a49
JonathanFL <j.lydemann@gmail.com>
2020-11-11 19:50:30 +0100

    Spiller

---
+++ angular-frontend/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WorkoutListComponent } from './workout-list/workout-list.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WorkoutListComponent,
+    LoginComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'workout-list', component: WorkoutListComponent},
+      {path: 'login', component: LoginComponent},
+      {path: '', redirectTo: '/workout-list', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
