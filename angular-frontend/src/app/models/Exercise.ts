abff5db226e128a60f824a3ada158a4751786c32
Mathias Nielsen <mathiasnielsen0@gmail.com>
2020-11-13 17:19:38 +0100

    fjernet workout modul

--- angular-frontend/src/app/models/Exercise.ts
+++ angular-frontend/src/app/models/Exercise.ts
@@ -1,5 +1,6 @@
 export default class Exercise {
     name: String;
+    _id: String;
     description: String;
     repetitions: Number;
     sets: Number;
--- angular-frontend/src/app/models/Workout.ts
+++ angular-frontend/src/app/models/Workout.ts
@@ -1,6 +1,7 @@
 import Exercise from '../models/Exercise'
 
 export default class Workout {
+    _id: string;
     name: String;
     exercises: [Exercise]
 }
