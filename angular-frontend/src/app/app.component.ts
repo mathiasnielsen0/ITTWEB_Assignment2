4627fa6985975029afdb74f5981fa52a8578ad35
JonathanFL <j.lydemann@gmail.com>
2020-11-13 18:01:43 +0100

    Update app.component.ts

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, OnChanges, SimpleChanges } from '@angular/core';
+import { Subject } from 'rxjs/internal/Subject';
 import {AuthService} from './AuthService/AuthService';
 
 @Component({
@@ -6,17 +7,23 @@ import {AuthService} from './AuthService/AuthService';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnChanges{
+export class AppComponent implements OnChanges, AfterContentChecked{
   title = 'WorkIt';
-  isLoggedIn:boolean = this.authService.isLoggedIn();;
+  isLoggedIn:boolean = this.authService.isLoggedIn();
+  isLoggedInChange: Subject<boolean> = new Subject<boolean>();
   constructor(private authService: AuthService) {
+    this.isLoggedInChange.subscribe((value) => {
+      this.isLoggedIn = value;
+    })
     console.log(authService.isLoggedIn());
   }
-  ngOnChanges(changes: SimpleChanges): void {
-    this.isLoggedIn = this.authService.isLoggedIn();
+  ngAfterContentChecked(): void {
+    this.isLoggedInChange.next(this.authService.isLoggedIn());
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
 
+  }
 
   logout(){
     this.authService.logout();
